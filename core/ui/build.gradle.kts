5b24488916290dbde9da16e50e9b23f0d2a1c41f
shenhk <shenhk@users.noreply.github.com>
2026-08-04 17:06:18 +0800

    重构：迁移核心通用组件

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -15,9 +15,11 @@ android {
 }
 
 dependencies {
+    api(project(":core:model"))
     api(platform(libs.androidx.compose.bom))
     api(libs.androidx.compose.material3)
     api(libs.androidx.compose.ui)
     api(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material.icons.extended)
 }
